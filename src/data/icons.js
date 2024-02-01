382f79e78f06d866c04d26f20f61a59ec7f4f561
Maximiliano Frega <maxifregawebdev@gmail.com>
2024-02-01 15:12:32 -0300

    seccion contacto terminada

---
+++ src/data/icons.js
@@ -0,0 +1,19 @@
+import Visa from '../assets/images/iconos/visa.svg'
+import Mastercard from '../assets/images/iconos/mastercard.svg'
+import Maestro from '../assets/images/iconos/maestro.svg'
+import Cabal from '../assets/images/iconos/cabal.svg'
+import Amex from '../assets/images/iconos/amex.svg'
+import Mercadopago from '../assets/images/iconos/mercadopago.svg'
+import Cuentadni from '../assets/images/iconos/cuentadni.svg'
+import Modo from '../assets/images/iconos/modologo.svg'
+
+export const  mediosDePago = [
+    Visa,
+    Mastercard,
+    Cabal,
+    Amex,
+    Maestro,
+    Mercadopago,
+    Cuentadni,
+    Modo
+];
--- src/sections/Contacto.js
+++ src/sections/Contacto.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import SectionContainer from '../hoc/SectionContainer'
 
+import { mediosDePago } from '../data/icons';
+
 import { FaPhone, FaLocationDot  } from "react-icons/fa6";
 import { MdOutlineWatchLater } from "react-icons/md";
 
 //poner horario, direccion, telefono, mapa, redes
 //agregar medios de pago
 //agregar responsividad combinada con el footer
+
+//INVERTIR COLORES ENTRE PORTADA Y CONTACTO PARA MEJORAR VISIBILIDAD DE LOS MEDIOS DE PAGO
 
 //falta MODO y CUENTA DNI en formas de pago
 
@@ -17,18 +21,20 @@ const Contacto = () => {
         <div className='info-contacto'>
           <h2>Contacto</h2>
           <div className='detalle-contacto'>
-            <p><FaLocationDot/> Dirección: Av. Álvarez Jonte 4994</p>
-            <p><FaPhone/> Teléfono: 4648-0107</p>
+            <p><FaLocationDot className='contact-icon'/> Dirección: Av. Álvarez Jonte 4994</p>
+            <p><FaPhone className='contact-icon'/> Teléfono: 4648-0107</p>
             <p style={{alignSelf: 'center'}}>Horarios</p>
-            <p><MdOutlineWatchLater/> Lun a Vie: 10hs a 13hs y 16hs a 20hs</p>
-            <p><MdOutlineWatchLater/> Sáb: 10hs a 14hs y 16hs a 20hs</p>
+            <p><MdOutlineWatchLater className='contact-icon'/> Lun a Vie: 10hs a 13hs y 16hs a 20hs</p>
+            <p><MdOutlineWatchLater className='contact-icon'/> Sáb: 10hs a 14hs y 16hs a 20hs</p>
           </div>
-          <div>
             <p>Formas de pago</p>
-            {/* agregar iconos de formas de pago */}
+          <div className='medios-de-pago'>
+            {mediosDePago.map((medio, index) => {
+              return <img className='medioIcon' src={medio} key={index} alt={`medio${index}`}/>
+            })}
           </div>
         </div>
-        <img src={require('../assets/images/mapa-contacto.png')} alt='mapa-contacto'/>
+        <img className='mapa-contacto' src={require('../assets/images/mapa-contacto.png')} alt='mapa-contacto'/>
       </div>
     </SectionContainer>
   )
