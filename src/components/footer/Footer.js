f12c81026d94360d2493e929523709e054044566
Maximiliano Frega <maxifregawebdev@gmail.com>
2024-02-01 23:49:13 -0300

    footer terminado salvo direcciones de links, y primera vista de la portada completada

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { FaInstagram,FaFacebookSquare } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <div className='footer-icons'>
+        <a href='#'><FaFacebookSquare/></a>
+        <a href='#'><FaInstagram/></a>
+      </div>
+      <p>Creado por: <a href='#Agregar link a pagina del local' target='_blank' rel="noreferrer noopener">Maximiliano Frega</a></p>
+    </footer>
   )
 }
 
--- src/sections/Portada.js
+++ src/sections/Portada.js
@@ -7,8 +7,14 @@ const Portada = () => {
   return (
     <SectionContainer claseSeccion={'sect-portada'} nombreSeccion={'portada'}>
       <div className='section sect-portada'>
-        Portada
+        <div className='info-portada'>
+          <h1>Maximiliano Hombres</h1>
+          <p>Desde 1999 en Monte Castro.</p>
+          <p>Somos una empresa familiar dedicada a ofrecer productos de calidad a nuestros clientes, acorde a lo que necesiten logrando que confien en nosotros a lo largo de más de 20 años en el barrio.</p>
+          <a className='btn-portada' href='#indumentaria'>Nuestro producto</a>
+        </div>
       </div>
+      <img className='img-portada' src={require('../assets/images/mapa-contacto.png')} alt=''/>
     </SectionContainer>
   )
 }
